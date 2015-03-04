e4646f8d893b5c89a42c6b1cb819bce369acd7cc
Joyce Echessa <jokhessa@yahoo.com>
2015-03-04 10:30:28 +0300

    Added a custom adapter

--- app/src/main/java/com/echessa/taskit/TaskListActivity.java
+++ app/src/main/java/com/echessa/taskit/TaskListActivity.java
@@ -1,24 +1,64 @@
 package com.echessa.taskit;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class TaskListActivity extends ActionBarActivity {
+
+    private static final String TAG = "TaskListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
 
-        String[] items = {"1", "2", "3"};
+        Task[] items = new Task[3];
+        items[0] = new Task();
+        items[0].setName("Task 1");
+        items[1] = new Task();
+        items[1].setName("Task 2");
+        items[1].setDone(true);
+        items[2] = new Task();
+        items[2].setName("Task 3");
 
-        ListView list = (ListView)findViewById(R.id.task_list);
-        list.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+        ListView listView = (ListView)findViewById(R.id.task_list);
+        listView.setAdapter(new TaskAdapter(items));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "Position clicked is " + position);
+            }
+        });
+    }
+
+    private class TaskAdapter extends ArrayAdapter<Task> {
+        TaskAdapter(Task[] tasks) {
+            super(TaskListActivity.this, R.layout.task_list_row, R.id.task_item_name, tasks);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+            Task task = getItem(position);
+            TextView taskName = (TextView)convertView.findViewById(R.id.task_item_name);
+            taskName.setText(task.getName());
+
+            CheckBox doneBox = (CheckBox)convertView.findViewById(R.id.task_item_done);
+            doneBox.setChecked(task.isDone());
+
+            return convertView;
+        }
     }
 
 
